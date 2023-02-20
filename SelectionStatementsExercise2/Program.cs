bb95ff1b89703c8af19b4e7d6d97776b609c60e5
Aaron Duke <aaron.duke0817@gmail.com>
2023-02-20 14:38:40 -0500

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+
+            var userinput = Console.ReadLine();
+
+            switch (userinput)
+            {
+                case "Science":
+                    Console.WriteLine("Science is a great subject!");
+                    break;
+
+                case "Math":
+                    Console.WriteLine("Math is cool!");
+                    break;
+
+                case "Social Studies":
+                    Console.WriteLine("I love Social Studies too!");
+                    break;
+
+                case "English":
+                    Console.WriteLine("English is a wonderful subject!");
+                    break;
+
+                case "History":
+                    Console.WriteLine("I also love learning about History!");
+                    break;
+
+                default:
+                    Console.WriteLine("Hmmm I'm not familiar with that one.");
+                    break;
+
+            }
         }
     }
 }
